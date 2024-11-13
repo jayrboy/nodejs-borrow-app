f1bb0fce7740b232189c2acc6b0efbe2d4995387
jayrboy <j.jayox_@hotmail.com>
2024-11-14 01:23:23 +0700

    add borrow

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -48,3 +48,53 @@ export const addMoney = (req, res) => {
     res.status(500).send('Unexpected server error');
   }
 };
+
+export const addBorrow = async (req, res) => {
+  try {
+    const { _id, money, update_by, user_id } = req.body;
+
+    // ยืมเงิน
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    // ลดเงินในบัญชีผู้ใช้
+    user.money -= money;
+    await user.save();
+
+    // ค้นหา TransactionType 2 ที่ตรงกับการ "ยืมเงิน"
+    const transactionType = await TransactionType.findOne({ type_id: 2 });
+    if (!transactionType) {
+      return res.status(400).send('Transaction type not found');
+    }
+
+    // สร้างธุรกรรมใหม่
+    const transaction = new Transaction({
+      user_id: user_id,
+      status: transactionType.type_name,
+      borrow_by: user.name,
+      money,
+      update_by,
+    });
+    await transaction.save();
+
+    // เพิ่มเงินในบัญชีผู้ใช้ที่ยืม
+    const userToUpdate = await User.findById(user_id);
+    if (!userToUpdate) {
+      return res.status(404).send('User to update not found');
+    }
+
+    userToUpdate.money += money;
+    await userToUpdate.save();
+
+    // ส่งผลลัพธ์กลับ
+    res.status(200).json({
+      message: 'Borrow added and transaction recorded successfully',
+      money: userToUpdate.money,
+    });
+  } catch (error) {
+    console.log({ message: error });
+    res.status(500).send('Unexpected server error');
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { getUsers, addMoney } from '../controllers/users-controller.js';
+import {
+  getUsers,
+  addMoney,
+  addBorrow,
+} from '../controllers/users-controller.js';
 
 const router = express.Router();
 
@@ -8,5 +12,8 @@ router.get('/users', getUsers);
 
 // POST: /api/add/money
 router.post('/add/money', addMoney);
+
+// POST: /api/add/borrow
+router.post('/add/borrow', addBorrow);
 
 export default router;
